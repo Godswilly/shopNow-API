edd8b5a1e7a4985f5571813efd6431817c4c9a98
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2022-02-25 18:30:25 +0100

    Add product model

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 module.exports = app;
---
+++ models/productModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide product name'],
+      maxlength: [50, 'Name cannot be more than 50 characters'],
+      minlength: [3, 'Name cannot be less than 3 characters'],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, 'Please select product category'],
+      enum: ['Vehicles', 'Fashion', 'Health & Beauty', 'Electronics', 'Mobile Phones & Tablets', 'Computers'],
+    },
+    slug: String,
+    price: {
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: [true, 'Please provide number of product in stock'],
+      default: 0,
+    },
+    dateAdded: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
+    images: [String],
+    averageRating: {
+      type: Number,
+      default: 0,
+      min: [1, 'Ratings must be above 1.0'],
+      max: [5, 'Ratings must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+  },
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
