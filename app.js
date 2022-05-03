3b02bc6722e7dcb824516973490059da4a56c657
“Godswilly” <kalu.agukalu@gmail.com>
2022-05-03 12:04:47 +0100

    Add user routes

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const morgan = require('morgan');
 const productRouter = require('./routes/productRoutes');
 const orderRouter = require('./routes/orderRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
 
 //routes
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/orders', orderRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
