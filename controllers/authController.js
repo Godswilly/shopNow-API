18b282489b6e018383513066db9efd1540f478fe
Godswilly <kalu.agukalu@gmail.com>
2022-05-08 14:35:37 +0100

    Update signup handler

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const User = require('../models/userModel');
+const asyncHandler = require('../utils/asyncHandler');
+const AppError = require('../utils/appError');
+
+
+exports.signup = asyncHandler(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role,
+  });
+});
