6a1721635308b5dc76acdba5956daa2364a319cb
Godswilly <kalu.agukalu@gmail.com>
2022-05-17 22:07:32 +0100

    Implement JWT sign token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,31 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const asyncHandler = require('../utils/asyncHandler');
 const ErrorHandler = require('../utils/errorHandler');
 
+const signToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+};
 
 exports.signup = asyncHandler(async (req, res, next) => {
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    role: req.body.role,
-  });
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+		role: req.body.role,
+	});
+
+	const token = signToken(newUser._id);
+
+	res.status(201).json({
+		status: 'success',
+		token,
+		data: {
+			user: newUser,
+		},
+	});
 });
